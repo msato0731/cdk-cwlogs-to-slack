277d3c6cf68f8017b213b0fa20ba2060697c22a1
msato0731 <rhcq.3127@gmail.com>
2021-01-16 07:54:14 +0900

    first commit

--- lib/cdk-cwlogs-to-slack-stack.ts
+++ lib/cdk-cwlogs-to-slack-stack.ts
@@ -1,9 +1,32 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as logs from '@aws-cdk/aws-logs'
+import * as logsDestinations from '@aws-cdk/aws-logs-destinations'
 
 export class CdkCwlogsToSlackStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const slackHookUrl = "<SlackWebhookURL>"
+    const slackChannel = "<SlackChannelID>"
+    const logGroup = logs.LogGroup.fromLogGroupName(this, "loggroup", "testmessage.log")
+    const filterPattern = logs.FilterPattern.allTerms("ERROR")
+
+    const fn = new lambda.Function(this, "lambda-function", {
+      runtime: lambda.Runtime.PYTHON_3_8,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'index.lambda_handler',
+      environment: {
+        "slackHookUrl": slackHookUrl,
+        "slackChannel": slackChannel
+      }
+    })
+
+    new logs.SubscriptionFilter(this, "subscription", {
+      logGroup: logGroup,
+      destination: new logsDestinations.LambdaDestination(fn),
+      filterPattern: filterPattern
+    })
+  
   }
 }
